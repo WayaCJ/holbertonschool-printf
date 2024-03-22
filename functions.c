ec0f8900be729b99392863134d21b75f5b3b39e8
Carlos <sotocarlos1956@gmail.com>
2024-03-21 23:51:56 -0400

    fixing betty doc

--- functions.c
+++ functions.c
@@ -21,7 +21,7 @@ int i_char(va_list args)
  */
 int i_string(va_list args)
 {
-	const char *str = va_arg(args, const char*);
+	const char *str = va_arg(args, const char *);
 	int count = 0;
 
 	while (*str)
